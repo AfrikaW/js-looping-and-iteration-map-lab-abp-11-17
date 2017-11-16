17f6d7558c18acdbffa1752287e8c7b0e872c1a2
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-16 02:09:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function(driver){
+    return driver.toLowerCase();
+
+
+  })
+
+}
+
+function nameToAttributes (drivers) {
+   return drivers.map(function (driver){
+    let fName = driver.split(' ')[0];
+    let lName = driver.split(' ')[1];
+    return { firstName: fName, lastName: lName};
+  });
+}
+
+function attributesToPhrase (drivers) {
+  return drivers.map(function (driver){
+
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
+
+
+
+//it('returns all drivers lowercased', function () {
+//  expect(lowerCaseDrivers(drivers)).to.eql(['bobby', 'sammy', 'sally', 'annette', 'sarah', 'bobby']);
+//});
+
+//const squaredNumbers = map(originalNumbers, function (num) { return num * num; });// Code your solution in this file.
+
+
+//function matchName(drivers, name) {
+  //return drivers.filter(function(driver){
+    //return name === driver.name
