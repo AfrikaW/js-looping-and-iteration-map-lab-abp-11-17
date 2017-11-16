36a3bd61248db1bfc2d22b1f05b6c65cae1065e9
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-16 01:59:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nameToAttributes (drivers) {
   });
 }
 
-function attributesToPhrase (driver) {
+function attributesToPhrase (drivers) {
   return drivers.map(function(driver){
     return {`${driver[name]} is from ${driver[hometown]}`};
   });
