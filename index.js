e90a6bbe32f7db7145e0c04c7f8cd57556c443ad
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-16 01:58:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nameToAttributes (drivers) {
 
 function nameToAttributes (driver) {
   return drivers.map(function(driver){
-    return {`${driver[name]} is from ${driver[hometown]}`}
+    return {`${driver[name]} is from ${driver[hometown]}`};
   });
 }
 
